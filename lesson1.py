a62d029657051d197708010c995b591fb5126202
Brady <brady.vardeman@gmail.com>
2019-11-14 21:23:51 -0700

    Night One

---
+++ lesson1.py
@@ -0,0 +1,21 @@
+#import sys
+
+#print ('Hello world')
+
+#one = 'this is a word'
+
+#print (one)
+
+number = 2
+number2 = 3
+floater = 1.75
+boolean = False
+
+if number == 100:
+	print ('fuck you')
+	
+elif not boolean:
+	print ('you bitch')
+
+else:
+	print ('finally you dumbass')
